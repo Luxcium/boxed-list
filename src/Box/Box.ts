da4fcb78eb641cd21be78171ac7c4d3d2efed4ca
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-04-16 23:28:59 -0400

    fix: :bug: fix a bug in test suit
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- src/Box/Box.ts
+++ src/Box/Box.ts
@@ -1,5 +1,8 @@
 import type { IApply, IBox, IChain, IMap, IUnbox, IValue } from '../types';
 
+/**
+ * This is the doc comment for Box class
+ */
 export default class Box<T>
   implements IApply<T>, IChain<T>, IMap<T>, IUnbox<T>, IBox<T>, IValue<T>
 {
@@ -41,13 +44,14 @@ export default class Box<T>
     return this.map(fn).value;
   }
 
+  public 'fantasy-land/map' = this.map;
   // public IMap<T> & 'fantasy-land/map' ==============-| map() |-====
-  public map<R>(fn: (value: T) => R) {
-    return Box.of(fn(this.#value));
+  public map<R>(fn: (value: T, index?: -1) => R) {
+    return Box.of(fn(this.#value, -1));
   }
 
   // public IUnbox<T> ===============================-| unbox() |-====
-  public unbox() {
+  public unbox(): T {
     return this.#value;
   }
 
